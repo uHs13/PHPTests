eca718afc716bb1bc4647a070a0f3236d89e1b74
uHs13 <heitorhenriquedias@gmail.com>
2021-10-06 17:06:41 -0300

    171006102121

--- application/src/OrderBundle/Test/Service/CustomerCategoryServiceTest.php
+++ application/src/OrderBundle/Test/Service/CustomerCategoryServiceTest.php
@@ -2,79 +2,100 @@
 
 namespace OrderBundle\Test\Service;
 
-use OrderBundle\Entity\Customer;
 use OrderBundle\Service\CustomerCategoryService;
+use OrderBundle\Service\MediumUserCategory;
 use OrderBundle\Service\LightUserCategory;
 use OrderBundle\Service\HeavyUserCategory;
-use OrderBundle\Service\MediumUserCategory;
 use OrderBundle\Service\NewUserCategory;
+use OrderBundle\Entity\Customer;
 use PHPUnit\Framework\TestCase;
 
 class CustomerCategoryServiceTest extends TestCase
 {
-    private $customerCategoryService;
+    private CustomerCategoryService $customerCategoryService;
+    private Customer $customer;
 
-    public function setup()
+    public function setUp(): void
     {
+        $this->customer = new Customer(
+            'name',
+            '5684712345',
+            true,
+            true
+        );
+
         $this->customerCategoryService = new CustomerCategoryService();
-        $this->customerCategoryService->addCategory(new HeavyUserCategory());
-        $this->customerCategoryService->addCategory(new MediumUserCategory());
-        $this->customerCategoryService->addCategory(new LightUserCategory());
-        $this->customerCategoryService->addCategory(new NewUserCategory());
+
+        $this->customerCategoryService
+        ->addCategory(new HeavyUserCategory());
+
+        $this->customerCategoryService
+        ->addCategory(new MediumUserCategory());
+
+        $this->customerCategoryService
+        ->addCategory(new LightUserCategory());
+
+        $this->customerCategoryService
+        ->addCategory(new NewUserCategory());
     }
 
     /**
      * @test
-     */
-    public function customerShouldBeNewUser()
-    {
-        $customer = new Customer();
-        $usageCategory = $this->customerCategoryService->getUsageCategory($customer);
+     * @dataProvider dataProvider
+     * */
+    public function shouldBeProperlyClassified(
+        int $orders,
+        int $ratings,
+        int $recommendations,
+        string $category
+    ): void {
 
-        $this->assertEquals(CustomerCategoryService::CATEGORY_NEW_USER, $usageCategory);
+        $this->customer->setTotalOrders($orders);
+        $this->customer->setTotalRatings($ratings);
+        $this->customer->setTotalRecommendations($recommendations);
+
+        $this->assertEquals(
+            $category,
+            $this->customerCategoryService
+            ->getUsageCategory($this->customer)
+        );
     }
 
     /**
-     * @test
-     */
-    public function customerShouldBeLightUser()
+     * @dataProvider
+     * */
+    public function dataProvider(): array
     {
-        $customer = new Customer();
-        $customer->setTotalOrders(5);
-        $customer->setTotalRatings(1);
-
-        $usageCategory = $this->customerCategoryService->getUsageCategory($customer);
-
-        $this->assertEquals(CustomerCategoryService::CATEGORY_LIGHT_USER, $usageCategory);
+        return [
+            'shouldBeANewUser' => [
+                'orders' => 0,
+                'ratings' => 0,
+                'recommendations' => 0,
+                'category' => CustomerCategoryService::CATEGORY_NEW_USER
+            ],
+            'shouldBeALightUser' => [
+                'orders' => 5,
+                'ratings' => 1,
+                'recommendations' => 0,
+                'category' => CustomerCategoryService::CATEGORY_LIGHT_USER
+            ],
+            'shouldBeAMediumUser' => [
+                'orders' => 20,
+                'ratings' => 5,
+                'recommendations' => 1,
+                'category' => CustomerCategoryService::CATEGORY_MEDIUM_USER
+            ],
+            'shouldBeAHeavyUser' => [
+                'orders' => 50,
+                'ratings' => 10,
+                'recommendations' => 5,
+                'category' => CustomerCategoryService::CATEGORY_HEAVY_USER
+            ],
+        ];
     }
 
-    /**
-     * @test
-     */
-    public function customerShouldBeMediumUser()
+    public function tearDown(): void
     {
-        $customer = new Customer();
-        $customer->setTotalOrders(20);
-        $customer->setTotalRatings(5);
-        $customer->setTotalRecommendations(1);
-
-        $usageCategory = $this->customerCategoryService->getUsageCategory($customer);
-
-        $this->assertEquals(CustomerCategoryService::CATEGORY_MEDIUM_USER, $usageCategory);
-    }
-
-    /**
-     * @test
-     */
-    public function customerShouldHeavyUser()
-    {
-        $customer = new Customer();
-        $customer->setTotalOrders(50);
-        $customer->setTotalRatings(10);
-        $customer->setTotalRecommendations(5);
-
-        $usageCategory = $this->customerCategoryService->getUsageCategory($customer);
-
-        $this->assertEquals(CustomerCategoryService::CATEGORY_HEAVY_USER, $usageCategory);
+        unset($this->customerCategoryService);
     }
 }
